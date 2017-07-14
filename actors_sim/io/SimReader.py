d09ebba39da8b0c8d5c980e038d6043e60d4dfe5
William Tärneberg <william@reklam.eit.lth.se>
2017-07-14 12:05:45 +0200

    Separated actors according to dependency [comedi, qpgen, sim]. Manage appropriate import through calvin.conf.

---
+++ actors_sim/io/SimReader.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from calvin.actor.actor import Actor, manage, condition, stateguard
+import posix_ipc as ipc
+
+class SimReader(Actor):
+
+    """
+    Reads process simulation from a message queue
+    Outputs:
+      value: Output value
+    """
+
+    @manage(['tick', 'device', 'timer', 'started', 'value', 'scale'])
+    def init(self, tick, device, scale):
+        self.tick = tick
+        self.device = device
+        self.value = 0
+        self.scale = scale
+
+        self.timer = None 
+        self.started = False 
+
+        self.setup()
+
+    def setup(self):
+        self.use('calvinsys.events.timer', shorthand='timer')
+        self.inqueue = ipc.MessageQueue(self.device, flags=ipc.O_CREAT, max_messages=1)
+
+    def start(self):
+        self.timer = self['timer'].repeat(self.tick)
+        self.started = True
+
+    def will_migrate(self):
+        self.inqueue.close()
+        if self.timer:
+            self.timer.cancel()
+
+    # Can't actually migrate
+    def did_migrate(self):
+        self.setup()
+        if self.started:
+            self.start()
+
+    def read(self):
+      try:        
+        message, priority = self.inqueue.receive(0) # Get input signal
+        self.value = max(-10.0, min(10.0, float(message)/self.scale*10.0))
+      except ipc.BusyError:
+        pass
+      return self.value
+
+    @stateguard(lambda self: not self.started)
+    @condition([], ['value'])
+    def start_timer(self):
+        value = self.read()
+        self.start()
+        return (value, )
+
+    @stateguard(lambda self: self.timer and self.timer.triggered)
+    @condition([], ['value'])
+    def trigger(self):
+        self.timer.ack()
+
+        return (self.read(), )
+
+    action_priority = (start_timer, trigger)
+    requires = ['calvinsys.events.timer']
---
+++ actors_sim/io/SimWriter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from calvin.actor.actor import Actor, manage, condition
+import posix_ipc as ipc
+import math
+
+class SimWriter(Actor):
+
+    """
+    Writes to process simulation through a message queue
+
+    Input:
+      value : Input value
+    """
+
+    @manage(['device',])
+    def init(self, device):
+        self.device = device
+        self.setup()
+
+    def setup(self):
+      self.outqueue = ipc.MessageQueue(self.device, flags=ipc.O_CREAT, max_messages=1)
+
+    # Can't actually migrate
+    def will_migrate(self):
+        self.outqueue.close()
+
+    def did_migrate(self):
+        self.setup()
+
+    @condition(action_input=("value",))
+    def write(self, value):
+        try:
+          self.outqueue.send("{}".format((value/10.0)*2*math.pi))
+        except ipc.BusyError:
+          print("Failed to set new input, this should not happen")
+
+    action_priority = (write, )
