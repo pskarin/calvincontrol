5113a0f38aa4d7791b5dc524dbd28042b8c533b5
William <william.tarneberg@eit.lth.se>
2017-07-27 10:14:48 +0200

    New random setpoint actors

---
+++ actors/misc/RandomIntervalRandomValueGenerator.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# William Tärneberg 2017
+from calvin.actor.actor import Actor, manage, condition, stateguard
+
+import random as rnd
+
+class RandomIntervalRandomValueGenerator(Actor):
+    """
+    Sequentially pass a value from __values__ at __tick__
+    Outputs:
+        value: a value from __values__
+    """
+
+    @manage(['tick_range', 'value_range', 'index', 'started'])
+    def init(self, tick_range, value_range):
+        self.tick_range = tick_range
+        self.value_range = value_range
+        self.index = 0 
+        self.timer = None
+        self.started = False
+        self.setup()
+
+    def setup(self):
+        self.use('calvinsys.events.timer', shorthand='timer')
+
+    def start(self):
+        self.timer = self['timer'].once( rnd.uniform( self.tick_range[0], self.tick_range[1]))
+        self.started = True
+
+    def will_migrate(self):
+        if self.timer:
+            self.timer.cancel()
+
+    def did_migrate(self):
+        self.setup()
+        if self.started:
+            self.start()
+
+    @stateguard(lambda self: not self.started)
+    @condition([], ['value'])
+    def start_timer(self):
+        self.start()
+
+        value = 0
+        
+        return (value, )
+
+    @stateguard(lambda self: self.timer and self.timer.triggered)
+    @condition([], ['value'])
+    def trigger(self):
+        self.timer.ack()
+        value = rnd.randint( self.value_range[0], self.value_range[1])
+
+        self.start()
+
+        return (value, )
+
+    action_priority = (start_timer, trigger)
+    requires = ['calvinsys.events.timer']
---
+++ actors/misc/RandomIntervalValueIterator.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# William Tärneberg 2017
+from calvin.actor.actor import Actor, manage, condition, stateguard
+
+import random as rnd
+
+class RandomIntervalValueIterator(Actor):
+    """
+    Sequentially pass a value from __values__ at __tick__
+    Outputs:
+        value: a value from __values__
+    """
+
+    @manage(['tick_range', 'values', 'index', 'started'])
+    def init(self, tick_range, values):
+        self.tick_range = tick_range
+        self.values = values
+        # Add exception for len(values) <= 2
+        self.index = 0 
+        self.timer = None
+        self.started = False
+        self.setup()
+
+    def setup(self):
+        self.use('calvinsys.events.timer', shorthand='timer')
+
+    def start(self):
+        self.timer = self['timer'].once( rnd.uniform( self.tick_range[0], self.tick_range[1]))
+        self.started = True
+
+    def will_migrate(self):
+        if self.timer:
+            self.timer.cancel()
+
+    def did_migrate(self):
+        self.setup()
+        if self.started:
+            self.start()
+
+    @stateguard(lambda self: not self.started)
+    @condition([], ['value'])
+    def start_timer(self):
+        self.start()
+
+        value = self.values[self.index]
+        self.index =  (self.index + 1)%len(self.values)
+
+        return (value, )
+
+    @stateguard(lambda self: self.timer and self.timer.triggered)
+    @condition([], ['value'])
+    def trigger(self):
+        self.timer.ack()
+        value = self.values[self.index]
+        
+        self.index =  (self.index + 1)%len(self.values)
+
+        self.start()
+
+        return (value, )
+
+    action_priority = (start_timer, trigger)
+    requires = ['calvinsys.events.timer']
