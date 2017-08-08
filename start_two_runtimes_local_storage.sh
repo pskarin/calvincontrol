c94d3311ea9b74ec0677218d50f4f4c16540e3a4
William <william.tarneberg@eit.lth.se>
2017-08-08 17:07:23 +0200

    Added shell script for starting two local storage runtimes and deoploying a distributed application

---
+++ start_two_runtimes_local_storage.sh
@@ -0,0 +1,12 @@
+ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+CALVIN_GLOBAL_STORAGE_TYPE=\"local\" csruntime -n $ip -p 5000 -c 5001 --name 'rt1' --probe-buffer-size 500000 >> rt1_output.actortrace &
+sleep 4
+
+CALVIN_GLOBAL_STORAGE_TYPE=\"proxy\" CALVIN_GLOBAL_STORAGE_PROXY=\"calvinip://$ip:5000\" csruntime -n $ip -p 5002 -c 5003 --name 'rt2' --probe-buffer-size 500000 >> rt2_output.actortrace &
+sleep 2
+
+cscontrol http://$ip:5001 deploy ball_n_beam-sim-pid.calvin --reqs ball_n_beam-sim-pid.deployjson
+sleep 3
+
+echo "Started two runtimes {rt1, rt2} on host:$ip, with storage on rt1."
