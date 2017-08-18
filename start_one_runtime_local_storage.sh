0a7c81779b76ef18e24894124a06dac6a3be50ff
William <william.tarneberg@eit.lth.se>
2017-08-18 11:26:42 +0200

    Scrip clean up

--- start_one_runtime_local_storage.sh
+++ start_one_runtime_local_storage.sh
@@ -2,6 +2,6 @@ ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
 echo $ip
 
-CALVIN_GLOBAL_STORAGE_TYPE=\"local\" csruntime -n $ip -p 5000 -c 5001 --name 'rt1' --probe-buffer-size 500000 >> rt1_output.actortrace &
+CALVIN_GLOBAL_STORAGE_TYPE=\"local\" csruntime -n $ip -p 5000 -c 5001 --name 'rt1' --probe-buffer-size 2000000 >> rt1_output.actortrace &
 
 echo "Started one runtime {rt1} on host:$ip, with local storage."
