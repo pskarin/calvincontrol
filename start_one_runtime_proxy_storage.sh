91542a9863c377025ad2f9faaa62b56f46749d55
William <william.tarneberg@eit.lth.se>
2017-08-18 10:34:39 +0200

    tweaked start script for proxy

--- start_one_runtime_proxy_storage.sh
+++ start_one_runtime_proxy_storage.sh
@@ -1,3 +1,5 @@
+rm rt2*
+
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
 CALVIN_GLOBAL_STORAGE_TYPE=\"proxy\" CALVIN_GLOBAL_STORAGE_PROXY=\"calvinip://$1:5000\" csruntime -n $ip -p 5002 -c 5003 --name 'rt2' --probe-buffer-size 500000 >> rt2_output.actortrace &
