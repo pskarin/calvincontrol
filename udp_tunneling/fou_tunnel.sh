18b1bd36f0aa47c45d291c9485fd2674709b7d4a
William <william.tarneberg@eit.lth.se>
2017-10-26 15:30:12 +0200

    Updated fou tunneling script

--- udp_tunneling/fou_tunnel.sh
+++ udp_tunneling/fou_tunnel.sh
@@ -1,11 +1,11 @@
 ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
-echo "Opening a UDP tunnel between $ip:$1 <-> $2:$1 using the interface:$3 with ip:$4" 
+echo "Opening a UDP tunnel between $ip:$1 <-> $2:$3 using the interface:$4 with ip:$5" 
 
 modprobe fou
 ip fou add port $1 ipproto 4
-ip link add name $3 type ipip \
+ip link add name $4 type ipip \
        remote $2 local $ip ttl 225 \
        encap fou \
-       encap-sport auto encap-dport $1
-ifconfig tunudp inet $4 netmask 255.255.255.0 up
+       encap-sport auto encap-dport $3
+ifconfig tunudp inet $5 netmask 255.255.255.0 up
