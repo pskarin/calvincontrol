de049795abb062a9b612568475cd24cd80b5b27d
Per Skarin <per.skarin@ericsson.com>
2017-12-12 16:16:32 +0100

    Update sim actors for clock

--- actors_sim/io/SimReader.py
+++ actors_sim/io/SimReader.py
@@ -4,71 +4,47 @@ import posix_ipc as ipc
 
 class SimReader(Actor):
 
-    """
-    Reads process simulation from a message queue
-    Outputs:
-      value: Output value
-    """
+	"""
+	Reads process simulation from a message queue
 
-    @manage(['tick', 'device', 'timer', 'started', 'value', 'scale'])
-    def init(self, tick, device, scale):
-        self.tick = tick
-        self.device = device
-        self.value = 0
-        self.scale = scale
+	Inputs:
+		tick: Clock tick
+	Outputs:
+		value: Output value
+	"""
 
-        self.timer = None 
-        self.started = False 
+	@manage(['device', 'value', 'scale'])
+	def init(self, device, scale):
+		self.device = device
+		self.value = 0
+		self.scale = scale
+		self.setup()
 
-        self.setup()
+	def setup(self):
+		self.inqueue = ipc.MessageQueue(self.device, flags=ipc.O_CREAT, max_messages=1)
+		self.use('calvinsys.native.python-time', shorthand='time')
+		self.time = self['time']
 
-    def setup(self):
-        self.use('calvinsys.events.timer', shorthand='timer')
-        self.inqueue = ipc.MessageQueue(self.device, flags=ipc.O_CREAT, max_messages=1)
+	def will_migrate(self):
+		self.inqueue.close()
 
-    def start(self):
-        self.timer = self['timer'].repeat(self.tick)
-        self.started = True
+	# Can't actually migrate
+	def did_migrate(self):
+		self.setup()
 
-    def will_migrate(self):
-        self.inqueue.close()
-        if self.timer:
-            self.timer.cancel()
+	def read(self):
+		try:        
+			message, priority = self.inqueue.receive(0) # Get input signal
+			self.value = max(-10.0, min(10.0, float(message)/self.scale*10.0))
+		except ipc.BusyError:
+			pass
+		return self.value
 
-    # Can't actually migrate
-    def did_migrate(self):
-        self.setup()
-        if self.started:
-            self.start()
+	@condition(['tick'], ['value'])
+	def trigger(self, tick):
+		value = self.read()
+		self.monitor_value = value 
+		return ((value, self.time.timestamp()),)
 
-    def read(self):
-      try:        
-        message, priority = self.inqueue.receive(0) # Get input signal
-        self.value = max(-10.0, min(10.0, float(message)/self.scale*10.0))
-      except ipc.BusyError:
-        pass
-      return self.value
-
-    @stateguard(lambda self: not self.started)
-    @condition([], ['value'])
-    def start_timer(self):
-        value = self.read()
-        self.start()
-
-	self.monitor_value = value
-
-        return (value, )
-
-    @stateguard(lambda self: self.timer and self.timer.triggered)
-    @condition([], ['value'])
-    def trigger(self):
-        self.timer.ack()
-
-	value = self.read()
-
-	self.monitor_value = value 
-
-        return (value, )
-
-    action_priority = (start_timer, trigger)
-    requires = ['calvinsys.events.timer']
+	action_priority = (trigger,)
+	requires = ['calvinsys.native.python-time']
