13d299c446ef5319a255041489e271a32c1e2534
Per Skarin <per.skarin@ericsson.com>
2017-12-12 16:28:17 +0100

    Update sim-pid and set SimWriter monitor value to delta time

--- actors_sim/io/SimWriter.py
+++ actors_sim/io/SimWriter.py
@@ -20,6 +20,8 @@ class SimWriter(Actor):
 
     def setup(self):
       self.outqueue = ipc.MessageQueue(self.device, flags=ipc.O_CREAT, max_messages=1)
+      self.use('calvinsys.native.python-time', shorthand='time')
+      self.time = self['time']
 
     # Can't actually migrate
     def will_migrate(self):
@@ -29,11 +31,13 @@ class SimWriter(Actor):
         self.setup()
 
     @condition(action_input=("value",))
-    def write(self, value):
+    def write(self, value_ts):
+        value, ts = value_ts
         try:
-          self.outqueue.send("{}".format((value[0]/10.0)*2*math.pi))
-          self.monitor_value = value
+          self.outqueue.send("{}".format((value/10.0)*2*math.pi))
+          self.monitor_value = self.time.timestamp()-ts
         except ipc.BusyError:
           sys.stderr.write("Failed to set new input, this should not happen\n")
 
     action_priority = (write, )
+    requires = ['calvinsys.native.python-time']
