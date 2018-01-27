d0948152ea9d54449239986c7980430fae069ce0
Per Skarin <per.skarin@ericsson.com>
2018-01-27 21:29:26 +0100

    Clock fixes and daily tweaks

--- actors/misc/Clock.py
+++ actors/misc/Clock.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from calvin.actor.actor import Actor, manage, condition, stateguard, calvinsys
-
+import sys
 from calvin.utilities.calvinlogger import get_actor_logger
 _log = get_actor_logger(__name__)
 
@@ -16,8 +16,11 @@ class Clock(Actor):
         _log.warning("Clock period: {}".format(period))
         self.period = period
         self.timer = calvinsys.open(self, "sys.timer.repeating")
+        self.use('calvinsys.native.python-time', shorthand='time')
+        self.time = self['time']
         self.started = False
         self.tick = 0
+        self.prev = 0
 
     @stateguard(lambda self: not self.started and calvinsys.can_write(self.timer))
     @condition([], ['tick'])
@@ -31,6 +34,9 @@ class Clock(Actor):
     def trigger(self):
         calvinsys.read(self.timer)
         self.tick += 1
+        t = self.time.timestamp()
+        sys.stderr.write("Tick {:0.3f}\n".format(t-self.prev))
+        self.prev = t
         return (self.tick, )
 
     action_priority = (start_timer, trigger)
