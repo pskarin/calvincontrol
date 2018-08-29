a6b9fa65acc03cdbad83673818b192aeb8056d54
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2018-08-29 17:34:40 +0200

    Delete tabs and fix PEP8 problems

--- actors/control/PID.py
+++ actors/control/PID.py
@@ -5,109 +5,115 @@ from calvin.actor.actor import Actor, manage, condition, stateguard, calvinsys
 from calvin.utilities.calvinlogger import get_actor_logger
 _log = get_actor_logger(__name__)
 
+
 class PID(Actor):
-	'''
-	Generic PID
-	
-	Inputs:
-		y: Measured value
-		y_ref: Reference point
-	Outputs:
-		v: Control value 
-	'''
+    '''
+    Generic PID
 
-	@manage(['td', 'ti', 'tr', 'kp', 'ki', 'kd', 'n' ,'beta', 'i', 'd', 'y_old', 'y_prev_t'])
-	def init(self, td=1., ti=5., tr=10., kp=-.2, ki=0., kd=0., n=10., beta=1.):
-		self.td = td
-		self.ti = ti
-		self.tr = tr
-		self.kp = kp
-		self.ki = ki 
-		self.kd = kd 
-		self.n = n
-		self.beta = beta
+    Inputs:
+        y: Measured value
+        y_ref: Reference point
+    Outputs:
+        v: Control value
+    '''
 
-		self.i = 0.
-		self.d = 0.
+    @manage(['td', 'ti', 'tr', 'kp', 'ki', 'kd', 'n', 'beta', 'i', 'd',
+             'y_old', 'y_prev_t'])
+    def init(self, td=1., ti=5., tr=10., kp=-.2, ki=0., kd=0., n=10., beta=1.):
+        self.td = td
+        self.ti = ti
+        self.tr = tr
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.n = n
+        self.beta = beta
 
-		self.y_old = 0.
+        self.i = 0.
+        self.d = 0.
 
-		self.time = None
-		self.setup()
-		self.y_prev_t = self.time.timestamp()
-    
-		self.yta = []
-		self.yt_refa = []
+        self.y_old = 0.
 
-	def setup(self):
-		self.use('calvinsys.native.python-time', shorthand='time')
-		self.time = self['time']
-		self.qt = self.time.timestamp()
+        self.time = None
+        self.setup()
+        self.y_prev_t = self.time.timestamp()
 
-	def did_migrate(self):
-		self.setup()
+        self.yta = []
+        self.yt_refa = []
 
-#	def pre_condition_wrapper(self):
-#		""" NOTE! The algorithm at this point assumes that there are only two queues! """
-#		isready = True
-#		keys = self.inports.keys()
-#		discarded = {}
-#		for k in keys: discarded[k] = 0    
-#		for xk,xv in self.inports.iteritems():
-#			if xv.num_tokens() > 0:
-#				top = xv.get_token_from_top(0).value
-#				for yk in [k for k in keys if not k == xk]:
-#					yv = self.inports[yk]
-#					cont = True
-#					while yv.num_tokens() > 1 and cont:
-#						yt0 = yv.get_token_from_top(0).value
-#						yt1 = yv.get_token_from_top(1).value
-#						if abs(yt0[1]-top[1]) > abs(yt1[1]-top[1]):
-#							yv.read()
-#							discarded[yk] += 1
-#						else:
-#							cont = False
-#			else:
-#				isready = False # Some queue is empty
-#		if isready:
-#			mintokens = min([x.num_tokens() for x in self.inports.values()])
-#			for xk in keys:
-#				xv = self.inports[xk]
-#				for r in range(0, mintokens-1):
-#					xv.read()
-#					discarded[xk] += 1
-#				self.log_queue_precond(xk, discarded[xk], xv.get_token_from_top(0).value[1])
+    def setup(self):
+        self.use('calvinsys.native.python-time', shorthand='time')
+        self.time = self['time']
+        self.qt = self.time.timestamp()
 
-	@condition(['y', 'y_ref'],['v'])
-	def cal_output(self, yt, yt_ref):
-		# Time management - for event based control
-		y_ref, ref_t, tick = yt_ref
-		y, y_t, _tick = yt
-		dt = y_t[0]-self.y_prev_t
-		self.y_prev_t = y_t[0]
+    def did_migrate(self):
+        self.setup()
 
-		# 
-		ad = self.td / (self.td + self.n*dt)
-		bd = self.kd * ad * self.n
+#    def pre_condition_wrapper(self):
+#        """
+# 		 NOTE! The algorithm at this point assumes that
+# 		 there are only two queues!
+# 		 """
+#        isready = True
+#        keys = self.inports.keys()
+#        discarded = {}
+#        for k in keys: discarded[k] = 0
+#        for xk,xv in self.inports.iteritems():
+#            if xv.num_tokens() > 0:
+#                top = xv.get_token_from_top(0).value
+#                for yk in [k for k in keys if not k == xk]:
+#                    yv = self.inports[yk]
+#                    cont = True
+#                    while yv.num_tokens() > 1 and cont:
+#                        yt0 = yv.get_token_from_top(0).value
+#                        yt1 = yv.get_token_from_top(1).value
+#                        if abs(yt0[1]-top[1]) > abs(yt1[1]-top[1]):
+#                            yv.read()
+#                            discarded[yk] += 1
+#                        else:
+#                            cont = False
+#            else:
+#                isready = False # Some queue is empty
+#        if isready:
+#            mintokens = min([x.num_tokens() for x in self.inports.values()])
+#            for xk in keys:
+#                xv = self.inports[xk]
+#                for r in range(0, mintokens-1):
+#                    xv.read()
+#                    discarded[xk] += 1
+#                self.log_queue_precond(xk, discarded[xk],
+# 									    xv.get_token_from_top(0).value[1])
 
-		# e
-		e = y_ref - y
+    @condition(['y', 'y_ref'], ['v'])
+    def cal_output(self, yt, yt_ref):
+        # Time management - for event based control
+        y_ref, ref_t, tick = yt_ref
+        y, y_t, _tick = yt
+        dt = y_t[0] - self.y_prev_t
+        self.y_prev_t = y_t[0]
 
-		# D
-		self.d = ad*self.d - bd*(y - self.y_old)
+        #
+        ad = self.td / (self.td + self.n * dt)
+        bd = self.kd * ad * self.n
 
-		# Control signal
-		v = self.kp * (self.beta * y_ref - y) + self.i + self.d
+        # e
+        e = y_ref - y
 
-		# I
-		self.i += (self.ki * dt/ self.ti) * e * (dt / self.tr) * (y-v)
+        # D
+        self.d = ad * self.d - bd * (y - self.y_old)
 
-		# Update state
-		self.y_old = y
+        # Control signal
+        v = self.kp * (self.beta * y_ref - y) + self.i + self.d
 
-		self.monitor_value = v
+        # I
+        self.i += (self.ki * dt / self.ti) * e * (dt / self.tr) * (y - v)
 
-		return ((v, y_t + ref_t, tick), )
+        # Update state
+        self.y_old = y
 
-	action_priority = (cal_output,)
-	requires = ['calvinsys.native.python-time']
+        self.monitor_value = v
+
+        return ((v, y_t + ref_t, tick), )
+
+    action_priority = (cal_output,)
+    requires = ['calvinsys.native.python-time']
--- actors/control/PIDClock.py
+++ actors/control/PIDClock.py
@@ -4,138 +4,144 @@ from calvin.actor.actor import Actor, manage, condition, stateguard, calvinsys
 from calvin.utilities.calvinlogger import get_actor_logger
 _log = get_actor_logger(__name__)
 
+
 class PIDClock(Actor):
-	'''
-	Generic PID
-	
-	Inputs:
-		y: Measured value
-		y_ref: Reference point
-	Outputs:
-		v: Control value 
-	'''
+    '''
+    Generic PID
 
-	@manage(['td', 'ti', 'tr', 'kp', 'ki', 'kd', 'n' ,'beta', 'i', 'd', 'y_old', 'y_prev_t','timer','period','started','tick'])
-	def init(self, td=1., ti=5., tr=10., kp=-.2, ki=0., kd=0., n=10., beta=1., period=0.05):
-		_log.warning("PID Clock period: {}".format(period))
-		self.td = td
-		self.ti = ti
-		self.tr = tr
-		self.kp = kp
-		self.ki = ki 
-		self.kd = kd 
-		self.n = n
-		self.beta = beta
-		self.period = period
+    Inputs:
+        y: Measured value
+        y_ref: Reference point
+    Outputs:
+        v: Control value
+    '''
 
-		self.i = 0.
-		self.d = 0.
+    @manage(['td', 'ti', 'tr', 'kp', 'ki', 'kd', 'n', 'beta', 'i', 'd',
+             'y_old', 'y_prev_t', 'timer', 'period', 'started', 'tick'])
+    def init(self, td=1., ti=5., tr=10., kp=-.2, ki=0., kd=0., n=10.,
+             beta=1., period=0.05):
+        _log.warning("PID Clock period: {}".format(period))
+        self.td = td
+        self.ti = ti
+        self.tr = tr
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.n = n
+        self.beta = beta
+        self.period = period
 
-		self.y_old = 0.
+        self.i = 0.
+        self.d = 0.
 
-		self.started = False
-		self.tick = 0
-		self.setup()
-		self.y_prev_t = self.time.timestamp()
+        self.y_old = 0.
 
-    
-		self.yta = []
-		self.yt_refa = []
+        self.started = False
+        self.tick = 0
+        self.setup()
+        self.y_prev_t = self.time.timestamp()
 
-	def setup(self):
-		self.timer = calvinsys.open(self, "sys.timer.repeating")
-		self.use('calvinsys.native.python-time', shorthand='time')
-		self.time = self['time']
-		self.qt = self.time.timestamp()
-                _log.warning("Set up")
-                Cont = calvinsys.can_write(self.timer)
-                if Cont == False:
-                    _log.warning("Can't write timer")
-                elif self.started == False:
-                    _log.warning("write timer")
+        self.yta = []
+        self.yt_refa = []
 
-	@stateguard(lambda self: not self.started and calvinsys.can_write(self.timer))
-	@condition([],[])
-	def start_timer(self):
-                _log.warning("Start Timer")
-		self.started = True
-		calvinsys.write(self.timer, self.period)
-		return (self.tick, )
+    def setup(self):
+        self.timer = calvinsys.open(self, "sys.timer.repeating")
+        self.use('calvinsys.native.python-time', shorthand='time')
+        self.time = self['time']
+        self.qt = self.time.timestamp()
+        _log.warning("Set up")
+        Cont = calvinsys.can_write(self.timer)
+        if not Cont:
+            _log.warning("Can't write timer")
+        elif not self.started:
+            _log.warning("write timer")
 
-	@stateguard(lambda self: calvinsys.can_read(self.timer))
-	@condition([], [])
-	def trigger(self):
-		_log.debug('Take values from buffer.')
-		calvinsys.read(self.timer)
-		self.tick += 1
-                _log.warning("Tick: {}".format(self.tick))
-                return (self.tick, )
+    @stateguard(lambda self: (not self.started
+                              and calvinsys.can_write(self.timer)))
+    @condition([], [])
+    def start_timer(self):
+        _log.warning("Start Timer")
+        self.started = True
+        calvinsys.write(self.timer, self.period)
+        return (self.tick, )
 
+    @stateguard(lambda self: calvinsys.can_read(self.timer))
+    @condition([], [])
+    def trigger(self):
+        _log.debug('Take values from buffer.')
+        calvinsys.read(self.timer)
+        self.tick += 1
+        _log.warning("Tick: {}".format(self.tick))
+        return (self.tick, )
 
-	def did_migrate(self):
-		self.setup()
+    def did_migrate(self):
+        self.setup()
 
-#	def pre_condition_wrapper(self):
-#		""" NOTE! The algorithm at this point assumes that there are only two queues! """
-#		isready = True
-#		keys = self.inports.keys()
-#		discarded = {}
-#		for k in keys: discarded[k] = 0    
-#		for xk,xv in self.inports.iteritems():
-#			if xv.num_tokens() > 0:
-#				top = xv.get_token_from_top(0).value
-#				for yk in [k for k in keys if not k == xk]:
-#					yv = self.inports[yk]
-#					cont = True
-#					while yv.num_tokens() > 1 and cont:
-#						yt0 = yv.get_token_from_top(0).value
-#						yt1 = yv.get_token_from_top(1).value
-#						if abs(yt0[1]-top[1]) > abs(yt1[1]-top[1]):
-#							yv.read()
-#							discarded[yk] += 1
-#						else:
-#							cont = False
-#			else:
-#				isready = False # Some queue is empty
-#		if isready:
-#			mintokens = min([x.num_tokens() for x in self.inports.values()])
-#			for xk in keys:
-#				xv = self.inports[xk]
-#				for r in range(0, mintokens-1):
-#					xv.read()
-#					discarded[xk] += 1
-#				self.log_queue_precond(xk, discarded[xk], xv.get_token_from_top(0).value[1])
+#    def pre_condition_wrapper(self):
+#        """
+# 		 NOTE! The algorithm at this point assumes
+# 		 that there are only two queues!
+# 		 """
+#        isready = True
+#        keys = self.inports.keys()
+#        discarded = {}
+#        for k in keys: discarded[k] = 0
+#        for xk,xv in self.inports.iteritems():
+#            if xv.num_tokens() > 0:
+#                top = xv.get_token_from_top(0).value
+#                for yk in [k for k in keys if not k == xk]:
+#                    yv = self.inports[yk]
+#                    cont = True
+#                    while yv.num_tokens() > 1 and cont:
+#                        yt0 = yv.get_token_from_top(0).value
+#                        yt1 = yv.get_token_from_top(1).value
+#                        if abs(yt0[1]-top[1]) > abs(yt1[1]-top[1]):
+#                            yv.read()
+#                            discarded[yk] += 1
+#                        else:
+#                            cont = False
+#            else:
+#                isready = False # Some queue is empty
+#        if isready:
+#            mintokens = min([x.num_tokens() for x in self.inports.values()])
+#            for xk in keys:
+#                xv = self.inports[xk]
+#                for r in range(0, mintokens-1):
+#                    xv.read()
+#                    discarded[xk] += 1
+#                self.log_queue_precond(xk, discarded[xk],
+#                                       xv.get_token_from_top(0).value[1])
 
-	@condition(['y', 'y_ref'],['v'])
-	def cal_output(self, yt, yt_ref):
-		# Time management - for event based control
-		y_ref, ref_t, tick = yt_ref
-		y, y_t, _tick = yt
-		dt = y_t[0]-self.y_prev_t
-		self.y_prev_t = y_t[0]
+    @condition(['y', 'y_ref'], ['v'])
+    def cal_output(self, yt, yt_ref):
+        # Time management - for event based control
+        y_ref, ref_t, tick = yt_ref
+        y, y_t, _tick = yt
+        dt = y_t[0] - self.y_prev_t
+        self.y_prev_t = y_t[0]
 
-		# 
-		ad = self.td / (self.td + self.n*dt)
-		bd = self.kd * ad * self.n
+        #
+        ad = self.td / (self.td + self.n * dt)
+        bd = self.kd * ad * self.n
 
-		# e
-		e = y_ref - y
+        # e
+        e = y_ref - y
 
-		# D
-		self.d = ad*self.d - bd*(y - self.y_old)
+        # D
+        self.d = ad * self.d - bd * (y - self.y_old)
 
-		# Control signal
-		v = self.kp * (self.beta * y_ref - y) + self.i + self.d
+        # Control signal
+        v = self.kp * (self.beta * y_ref - y) + self.i + self.d
 
-		# I
-		self.i += (self.ki * dt/ self.ti) * e * (dt / self.tr) * (y-v)
+        # I
+        self.i += (self.ki * dt / self.ti) * e * (dt / self.tr) * (y - v)
 
-		# Update state
-		self.y_old = y
+        # Update state
+        self.y_old = y
 
-		self.monitor_value = v
+        self.monitor_value = v
 
-		return ((v, y_t + ref_t, self.tick), )
+        return ((v, y_t + ref_t, self.tick), )
 
-	action_priority = (start_timer,trigger,cal_output,)
-	requires = ['calvinsys.native.python-time','sys.timer.repeating']
+    action_priority = (start_timer, trigger, cal_output)
+    requires = ['calvinsys.native.python-time', 'sys.timer.repeating']
